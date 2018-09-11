979bb7de021e99222f31531db65c0067d1e5056b
Ramon <ramon_gill_8@hotmail.com>
2018-09-11 09:39:39 +0200

    added name (#11)

--- main.cpp
+++ main.cpp
@@ -4,14 +4,8 @@ using namespace std;
 int main() 
 {
     cout << "Albert van Kiel, 500729041";
-	cout << "Max Mallon is in da house 500740315";
-    cout << "Bram Tukker" << endl;
-    cout << "Hello, Bootcamp! - Geoffrey";
-    cout << "Ik ben Max van Hasselt";
-    cout << "Hello, Bootcamp!";
-    cout << "This is Eloy";    
-    cout << "Robin Baneke" << endl;
-    cout << "Albert van Kiel, 500729041";
-    cout << "Amin Chihab";
+
+    cout << "Ramon Gill, 500733062"
+
     return 0;
 }
